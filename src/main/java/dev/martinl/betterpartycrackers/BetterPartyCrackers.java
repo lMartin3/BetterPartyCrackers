e276791f00e65b7521ef8c3cafd76134c82dfadc
lMartin3 <martinlufr@gmail.com>
2022-03-30 11:40:40 -0300

    Initial commit

---
+++ src/main/java/dev/martinl/betterpartycrackers/BetterPartyCrackers.java
@@ -0,0 +1,17 @@
+package dev.martinl.betterpartycrackers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterPartyCrackers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
